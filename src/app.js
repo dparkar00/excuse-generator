cfc4bfaeb7b7cb751281f679cb5867b0b3e812e4
dparkar00 <67346510+dparkar00@users.noreply.github.com>
2024-03-13 23:46:18 +0000

    finished excuse generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  // Define what a sentence looks like
+  let sentence =
+    who[rand(who.length)] + // each array's index will be a random number from
+    " " +
+    action[rand(action.length)] +
+    " " +
+    what[rand(what.length)] +
+    " " +
+    when[rand(when.length)];
+  // Query the span by ID
+  let excuseSpan = document.querySelector("#excuse");
+
+  // Set the span's innerHTML to the sentance
+  excuseSpan.innerHTML = sentence;
 };
+
+function rand(length) {
+  // Given the length, randomize a number between 0 and the length of the array.
+  let number = Math.floor(Math.random() * length);
+  return number;
+}
